a605b81de81aec0bb2821d0b791d177be4fdce2c
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2023-06-23 23:46:50 +0300

    [5] – Contracts of Data Sources, https://resocoder.com/2019/09/12/flutter-tdd-clean-architecture-course-5-contracts-of-data-sources/

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:number_trivia/core/failures.dart';
+import 'package:number_trivia/core/error/failures.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
